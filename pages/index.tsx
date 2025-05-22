0f821c05c94ce1dde6fed11d9799f69aa5df1382
Chaewon Jeong <fromnowwon@gmail.com>
2025-05-22 11:53:04 +0900

    feat: 이미지 및 영역 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,46 @@ import SajuTable from "@/components/saju/SajuTable";
 export default function Home() {
   return (
     <main className="max-w-md w-full mx-auto pb-20 bg-gray-light">
-      <section className="w-full pt-21">
-        <div className="flex flex-col items-center gap-3">
-          <p className="text-white leading-[100%] tracking-[-0.005em]">
-            제 1장
-          </p>
+      <h1 className="sr-only">제 1장 나의 사주 팔자</h1>
+
+      <section className="w-full pb-23.5">
+        <div className="relative w-full aspect-[375/652]">
           <Image
-            src="/assets/images/line.svg"
-            width={157}
-            height={20}
-            alt="line"
+            src="/assets/images/key_visual.jpg"
+            fill
+            priority
+            alt="제 1장 나의 사주 팔자 메인 이미지"
+            className="object-cover"
+          />
+          <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-b from-transparent to-[#F3F2EF] pointer-events-none" />
+        </div>
+      </section>
+
+      <section className="relative h-86.25 pt-5 pb-10">
+        <div className="absolute right-6 bottom-10">
+          <Image
+            src="/assets/images/scene1.png"
+            width={161}
+            height={285}
+            alt="제 1장 나의 사주 팔자 장면 1"
+          />
+        </div>
+        <div className="absolute left-0 bottom-19.5">
+          <Image
+            src="/assets/images/deco1.svg"
+            width={153}
+            height={120}
+            alt="icon"
             aria-hidden="true"
           />
-          <h1 className="text-white leading-[100%] tracking-[-0.005em]">
-            나의 사주 팔자
-          </h1>
         </div>
       </section>
 
       <section>
-        <div className="relative w-full aspect-[375/306]">
+        <div className="relative aspect-[375/306]">
           <Image
             src="/assets/images/scene2.jpg"
-            alt="scene"
+            alt="제 1장 나의 사주 팔자 장면 2"
             fill
             className="object-cover"
           />
