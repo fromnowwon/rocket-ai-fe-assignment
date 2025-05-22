6dd3726b18ddb1762c923b900a3374cb5a250745
Chaewon Jeong <fromnowwon@gmail.com>
2025-05-22 11:22:08 +0900

    feat: 이미지 및 영역 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
+import Image from "next/image";
 import SajuTable from "@/components/saju/SajuTable";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center max-w-md w-full mx-auto px-3 pb-20 bg-gray-light">
-      <section className="relative flex flex-col items-center w-full max-w-87.75 pt-10 pb-8.25 bg-ivory border-3 border-navy-deep shadow-custom">
+    <main className="max-w-md w-full mx-auto pb-20 bg-gray-light">
+      <section className="w-full pt-21">
+        <div className="flex flex-col items-center gap-3">
+          <p className="text-white leading-[100%] tracking-[-0.005em]">
+            제 1장
+          </p>
+          <Image
+            src="/assets/images/line.svg"
+            width={157}
+            height={20}
+            alt="line"
+            aria-hidden="true"
+          />
+          <h1 className="text-white leading-[100%] tracking-[-0.005em]">
+            나의 사주 팔자
+          </h1>
+        </div>
+      </section>
+
+      <section>
+        <div className="relative w-full aspect-[375/306]">
+          <Image
+            src="/assets/images/scene2.jpg"
+            alt="scene"
+            fill
+            className="object-cover"
+          />
+          <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-b from-transparent to-[#F3F2EF] pointer-events-none" />
+        </div>
+      </section>
+
+      <section className="relative flex flex-col items-center w-full max-w-87.75 mx-auto -mt-[23px] pt-10 pb-8.25 bg-ivory border-3 border-navy-deep shadow-custom">
         <h2 className="text-center text-darkGray leading-[100%]">
           김로켓님의 사주
         </h2>
